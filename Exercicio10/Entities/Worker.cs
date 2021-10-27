cc836e20b1df3e774759aaec6f004a82fc9a1ce5
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2021-10-26 23:00:39 -0300

    Commit do projeto inteiro

---
+++ Exercicio10/Entities/Worker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Exercicio10.Entities.Enums;
+namespace Exercicio10.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; } //Associação entre classes e Enum
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; } //Associação entre classes diferentes 1 pra 1
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();//Associação de classes diferentes 1 pra muitos onde foi criada uma lista e instanciado a uma lista vazia
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department) //Nao incluir no construtor um para muitos, no caso a lista
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;    
+        }
+
+        public void AddContract(HourContract contract) //Recebe uma objeto do tipo HourContract
+        {
+            Contracts.Add(contract); //Adiciona esse objeto a Lista Contracts
+        }
+
+        public void RemoveContract(HourContract contract) //Recebe uma objeto do tipo HorContract
+        {
+            Contracts.Remove(contract);  //Remove esse objeto a Lista Contracts
+        }
+
+        public double Income(int year, int month) //Metodo que recebe um ano e um mês
+        {
+            double sum = BaseSalary; 
+            foreach (HourContract contract in Contracts) //Para cada contrato na lista de Contratos
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month) //Se o ano e o mês forem iguais ao parâmetro recebido
+                {
+                    sum += contract.TotalValue(); //Soma ao valor do contrato utilizando o metodo TotalValue
+                }
+            }
+            return sum; //Retorna a soma
+        }
+    }
+}
---
+++ Exercicio10/Program.cs
@@ -0,0 +1,51 @@
+﻿using Exercicio10.Entities;
+using Exercicio10.Entities.Enums    ;
+using System;
+using System.Globalization;
+
+namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter departament's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter Worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Junior/MidLevel/Senior: ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine()); //Recebe uma variável enum convertendo para string
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName); //Instanciando objeto departamento
+            Worker worker = new Worker(name, level, baseSalary, dept); //Instancia o objeto Worker passando como parâmetro o objeto dept
+            
+            
+            Console.WriteLine("How many contract to this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #" + i + " contract data:");
+                Console.Write("Date: (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHouer = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHouer, hours); //Passa parâmetros para o construtor HoursContract  
+                worker.AddContract(contract); //método do Worker que adiciona os contratos 
+            }
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2)); //Recorta a string e armazena na variável int
+            int year = int.Parse(monthAndYear.Substring(3));//Recorta a string e armazena na variável mês
+            Console.WriteLine("Name: " + worker.Name); //Chama o objeto Worker e imprime o nome
+            Console.WriteLine("Department: " + worker.Department.Name);//Chama o objeto worker que chama o objeto Department e imprime o nome do departamento.
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture)) ; //Chama o método Income da classe Worker do objeto worker pasasando como parâmetro o ano e o mês  
+        }
+    }
+}
